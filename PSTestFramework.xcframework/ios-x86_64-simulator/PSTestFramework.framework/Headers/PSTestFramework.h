b8ec708c6b0044d5fdc8516e2246919fdea17150
Pradeep Singh <prdpsingh402@gmail.com>
2020-04-03 00:01:17 +0530

    Add XCFramework

---
+++ PSTestFramework.xcframework/ios-x86_64-simulator/PSTestFramework.framework/Headers/PSTestFramework.h
@@ -0,0 +1,19 @@
+//
+//  PSTestFramework.h
+//  PSTestFramework
+//
+//  Created by Pradeep Singh on 31/03/20.
+//  Copyright © 2020 StarInfoLabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PSTestFramework.
+FOUNDATION_EXPORT double PSTestFrameworkVersionNumber;
+
+//! Project version string for PSTestFramework.
+FOUNDATION_EXPORT const unsigned char PSTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PSTestFramework/PublicHeader.h>
+
+
